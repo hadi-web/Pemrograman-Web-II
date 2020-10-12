d2eb033db3f8d11bb64ba894aa01e2c73785fc51
hadi-cyber <hadikusumanto2000@gmail.com>
2020-10-12 20:12:45 +0700

    PRAKTIKUM 4

---
+++ praktikum 4/v_footer.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+	<div>
+<footer>
+	<a href="http://printjagad.000webhostapp.com">WEB JAGAD PRINT</a>
+	<a href="http://freedesain.000webhostapp.com">WEB FREE DESAIN</a>
+</footer>
+	</div>
+</body>
+</html>
