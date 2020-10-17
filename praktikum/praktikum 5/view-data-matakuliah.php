9fe28a52e298e5225a8d9a36eb95cb50839c2d4e
hadi-cyber <hadikusumanto2000@gmail.com>
2020-10-17 10:46:58 +0700

    UPDATE

---
+++ praktikum/praktikum 5/view-data-matakuliah.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tampil Data Matakuliah</title>
+</head>
+
+<body>
+	<center>
+		<table>
+			<tr>
+				<th colspan="3">
+					Tampil Data Matakuliah
+				</th>
+			</tr>
+
+			<tr>
+				<th colspan="3">
+				</th>
+			</tr>
+
+			<tr>
+				<th>Kode MATKUL</th>
+				<td>:</td>
+				<td>
+					<?= $kode; ?>
+				</td>
+			</tr>
+
+			<tr>
+				<th>Nama MATKUL</th>
+				<td>:</td>
+				<td>
+					<?= $nama; ?>
+				</td>
+			</tr>
+
+			<tr>
+				<th>SKS</th>
+				<td>:</td>
+				<td>
+					<?= $sks; ?>
+				</td>
+			</tr>
+			<tr>
+				<th colspan="3" align="center">
+					<a href="<?= base_url('matakuliahvalidation');?>">Kembali</a>
+				</th>
+			</tr>
+
+		</table>
+
+	</form>
+</center>
+</body>
+</html>
